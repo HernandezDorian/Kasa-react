c7e405999d3494a724f444d89943f7f25b59b682
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-12-25 13:58:54 +0100

    Router configuré et pages vide créés

---
+++ src/Pages/Housing/index.jsx
@@ -0,0 +1,5 @@
+function Housing() {
+  return <h1>Hello Housing</h1>;
+}
+
+export default Housing;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+import About from "./Pages/About";
+import Housing from "./Pages/Housing";
 
 const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing" element={<Housing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
