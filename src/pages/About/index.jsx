90902f0420d140a6d9849d97e32a9cf81cafbf62
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-12-29 14:25:42 +0100

    About finish, error page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,22 +1,47 @@
 import "./index.scss";
 import Collapse from "../../components/Collapse";
+import Imgbar from "../../components/Imgbar";
+import background from "../../assets/BgAbout.jpg";
 
 function About() {
   return (
     <div className="About">
-      <h1>Hello About</h1>
-      <Collapse className={"About__Collapse"} Title="Test">
-        <ul>
-          <li>Content Test</li>
-          <li>Content Test</li>
-        </ul>
-      </Collapse>
-      <Collapse className={"About__Collapse"} Title="Test2">
-        <ul>
-          <li>Content Test 2</li>
-          <li>Content Test 2</li>
-        </ul>
-      </Collapse>
+      <Imgbar className="About__Imgbar" text="" img={background} />
+      <div className="About__CollapseList">
+        <Collapse className={"About__CollapseList__Collapse"} Title="Fiabilité">
+          <ul>
+            <li>Content Test</li>
+            <li>Content a Test</li>
+          </ul>
+        </Collapse>
+        <Collapse className={"About__CollapseList__Collapse"} Title="Respect">
+          <ul>
+            <li>Content aa Test 2</li>
+            <li>Content Test 2</li>
+            <li>Content Test 2</li>
+          </ul>
+        </Collapse>
+        <Collapse className={"About__CollapseList__Collapse"} Title="Service">
+          <ul>
+            <li>Content Test 3</li>
+            <li>Content aaa aaaaa aaaa Test 3</li>
+            <li>Content a Test 3</li>
+            <li>Content Test 3</li>
+          </ul>
+        </Collapse>
+        <Collapse className={"About__CollapseList__Collapse"} Title="Sécurité">
+          <ul>
+            <li>Content aaaa Test 4</li>
+            <li>Content aaaa aa Test 4</li>
+            <li>Content Test 4</li>
+            <li>Content aa aaaaATest 4</li>
+            <li>Content Test 4</li>
+            <li>Content Test 4</li>
+            <li>Content AAA AAAA aaaTest 4</li>
+            <li>Content aaa aaa aaaaaaa Test 4</li>
+          </ul>
+        </Collapse>
+      </div>
     </div>
   );
 }
--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,38 @@
+import "./index.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as SVGbig } from "./../../assets/404big.svg";
+import { ReactComponent as SVGsmall } from "./../../assets/404small.svg";
+import React, { useState, useEffect } from "react";
+
 function Error() {
-  return <h1>404</h1>;
+  const [isOnMobile, setisOnMobile] = useState(window.innerWidth < 630);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setisOnMobile(window.innerWidth < 630);
+    };
+
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  return (
+    <div className="Error">
+      {isOnMobile ? (
+        <SVGsmall className="Error__404" />
+      ) : (
+        <SVGbig className="Error__404" />
+      )}
+
+      <h2 className="Error__Oops">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link className="Error__Link" to="/">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
